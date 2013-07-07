fb10fc6981591ca98be22b3fb8c0c79e90e3b1ca
Paul Vorbach <paul@vorba.ch>
2013-07-07 21:46:25 +0200

    ScalaDoc

--- src/main/scala/de/vorb/text/pandoc/Pandoc.scala
+++ src/main/scala/de/vorb/text/pandoc/Pandoc.scala
@@ -8,21 +8,66 @@ import java.util.ArrayList
 import scala.annotation.implicitNotFound
 import scala.concurrent.{ ExecutionContext, Future, future }
 
+/**
+ * Wraps the Pandoc executable.
+ */
 object Pandoc {
+
+  /**
+   * Call Pandoc with the arguments specified in `args`.
+   *
+   * E.g. for `Pandoc(List("-f", "html", "-t", "markdown"))`
+   * `pandoc -f html -t markdown` will be run.
+   *
+   * @param args sequence of arguments for the command line. If an output file
+   *             is specified, execution will fail.
+   * @param ctx  execution context
+   *
+   * @return resulting document as a `Future[String]`
+   */
   def apply(args: Seq[String])(implicit ctx: ExecutionContext): Future[String] =
-    readOutputAsString(new ProcessBuilder(command(args)).start())
+    readOutputAsString(new ProcessBuilder(commandFor(args)).start())
 
-  def apply(from: InputFormat = MarkupFormat.Markdown,
-    to: OutputFormat = MarkupFormat.HTML,
-    in: Path)(implicit ctx: ExecutionContext): Future[String] =
-    apply("-f" :: from.name :: "-t" :: to.name :: in.toString :: Nil)
+  /**
+   * Call Pandoc with the given input format, output format and source file
+   * path.
+   *
+   * @param from input format
+   * @param to   output format
+   * @param in   path to source file
+   * @param additionalParameters additional parameters
+   * @param ctx  execution context
+   *
+   * @return resulting document as a `Future[String]`
+   */
+  def apply(from: InputFormat, to: OutputFormat, in: Path,
+    additionalArguments: String*)(
+      implicit ctx: ExecutionContext): Future[String] =
+    apply("-f" :: from.toString :: "-t" :: to.toString :: in.toString ::
+      additionalArguments.toList)
 
-  def apply(from: InputFormat,
-    to: OutputFormat,
-    in: InputStream)(implicit ctx: ExecutionContext): Future[String] = {
+  /**
+   * Call Pandoc with the given input format, output format and an input stream.
+   *
+   * @param from input format
+   * @param to   output format
+   * @param in   input stream
+   * @param additionalParameters additional parameters
+   * @param ctx  execution context
+   *
+   * @return resulting document as a `Future[String]`
+   */
+  def apply(from: InputFormat, to: OutputFormat, in: InputStream,
+    additionalArguments: String*)(
+      implicit ctx: ExecutionContext): Future[String] = {
 
-    val process = new ProcessBuilder(command(from, to)).start()
+    val cmd = commandFor(from, to)
+    additionalArguments.foreach(cmd.add(_)) // add additional parameters
 
+    val process = new ProcessBuilder(cmd).start()
+
+    // future that writes the characters from the input stream to the output
+    // stream
     future {
       val reader = new BufferedReader(new InputStreamReader(in, "UTF-8"))
       val writer = new BufferedWriter(new OutputStreamWriter(
@@ -61,20 +106,20 @@ object Pandoc {
     }
   }
 
-  private def command(args: Seq[String]) = {
+  private def commandFor(args: Seq[String]) = {
     val cmd = new ArrayList[String](args.length + 1)
     cmd.add("pandoc")
     args.foreach(cmd.add(_))
     cmd
   }
 
-  private def command(from: InputFormat, to: OutputFormat) = {
+  private def commandFor(from: InputFormat, to: OutputFormat) = {
     val cmd = new ArrayList[String](5)
     cmd.add("pandoc")
     cmd.add("-f")
-    cmd.add(from.name)
+    cmd.add(from.toString)
     cmd.add("-t")
-    cmd.add(to.name)
+    cmd.add(to.toString)
     cmd
   }
 }
